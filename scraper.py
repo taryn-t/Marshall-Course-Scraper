a1b97b7a273bf5865373b4077427343d14fb7906
Taryn Thompson <78978422+taryn-t@users.noreply.github.com>
2025-02-16 19:29:36 -0500

    comments added

--- scraper.py
+++ scraper.py
@@ -2,28 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
+#empty dictionary we will use to write to json file
 json_object = {
     "data":[]
 }
 
+#empty list for appending subject data to
 data =[]
+
 # create webdriver object 
 driver = webdriver.Firefox() 
 
-# get google.co.in 
+#get the course page from the schedule of courses
 driver.get("https://mubert.marshall.edu/scheduleofcourses.php?term=202502") 
 
+#grabs all elements with the class 'subjects'
 parentElements = driver.find_elements(By.CLASS_NAME, "subjects")
 
+#gets the second element in the subjects list which contains all the links to undergraduate courses
 parentElement = parentElements[1]
+
+#gets all the link elements in the section
 linkElements = parentElement.find_elements(By.TAG_NAME, "a")
 
-dept_urls =[]
+#empty list for adding the true urls
+dept_urls = []
 
-    
-# dept_urls= [elem.get_attribute('href')  for elem in linkElements]
+#get length of links    
 link_list_length = len(linkElements)
 
+#collects the true url with search params
 for i in range(link_list_length):
     linkElements[i].click()
     
@@ -32,21 +40,21 @@ for i in range(link_list_length):
     
     driver.back()
     
+    #refresh the original list
     parentElements = driver.find_elements(By.CLASS_NAME, "subjects")
     parentElement = parentElements[1]
     linkElements = parentElement.find_elements(By.TAG_NAME, "a")
   
 
-
-print(dept_urls)
-
-#each department
+#find each department/subject
 for url in dept_urls:
     print(url)
+    
     department = {
         "name": "",
         "courses": []
     }
+    
     name_header = driver.find_element(By.TAG_NAME, "h3").text
     
     department["name"] = name_header[:3]
@@ -58,7 +66,7 @@ for url in dept_urls:
     
     course_headers = driver.find_elements(By.TAG_NAME, "h4")
     
-    #each course
+    #find each course in dept/subject
     for header in course_headers:
         
         sections = []
@@ -73,12 +81,12 @@ for url in dept_urls:
         
         course_table = driver.execute_script(
         "return arguments[0].nextElementSibling;", main_parent)
-        # course_table = main_parent.find_element(By.XPATH, "following-sibling::*[1]")
+
         table_body = course_table.find_element(By.XPATH, "./tbody")
         
         table_rows = table_body.find_elements(By.XPATH, "./tr")
         
-        
+        #find each section in specific course
         for row in table_rows:
             table_data = row.find_elements(By.XPATH, "./td")
             if len(table_data) == 8:
@@ -87,11 +95,16 @@ for url in dept_urls:
         
                 course["sections"] = sections
                 
+        #add course to total courses        
         courses.append(course)
     
+    #set course data for the department
     department["courses"] = courses
+    
+    #add department to the data list
     data.append(department)
 
+#when loop is complete it 
 json_object["data"] = data
     
 
